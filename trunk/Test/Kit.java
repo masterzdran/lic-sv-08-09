05c065050ad6ef023c0ca095509518fc622d8252
Nuno.Cancelo <Nuno.Cancelo@98694540-1dcb-11de-b650-e715bd6d7cf1>
2009-07-20 09:39:18 +0000

    Organização de Pastas (Passo 3)

---
+++ trunk/Test/Kit.java
@@ -0,0 +1,131 @@
+import isel.leic.usbio.InputPort;
+import isel.leic.usbio.UsbPort;
+/*
+ * 
+ * 31401 - Nuno Cancelo 
+ * 31900 - José Guilherme
+ * 33595 - Nuno Sousa
+ * 
+ */
+public class Kit implements KitConstants{
+	private  int readInput;
+	private  int writeOutput;
+	
+	/**
+	 * Construtor da Class Kit. 
+	 * Inicia os valores dos portos de Input e OutPut.
+	 */
+	public Kit(){
+		readInput=0x00;
+		writeOutput=0x00;
+		write(writeOutput, EIGHTBITS);
+	}
+
+	/** 
+	 * Lê o input port para posterior consulta 
+	 * */
+	public void read() {
+		readInput =~( InputPort.in());
+	}
+
+	/**
+	 * Consulta o valor representado pelos bits da mascara que foram previamente
+	 * lidos com read() Retorna o valor correspondente dos bits com 0 para GND e
+	 * 1 para Vcc Por exemplo retorna 0x0F se I0..I3 estão a Vcc e I4..I7 estão
+	 * a GND
+	 */
+	public  int getBits(int mask) {
+		return (readInput&mask);
+	}
+
+	/**
+	 * Consulta o bit indicado pela mascara previamente lido com read() Retorna
+	 * true se o bit indicado pela mascara está a Vcc
+	 */
+	public  boolean isBit(int mask) {
+		return ((readInput&mask)==mask);
+	}
+
+	/** 
+	 * Lê e consulta o valor representado pelos bits da mascara 
+	 * */
+	public  int readBits(int mask) {
+		read();
+		return getBits(mask);
+	}
+
+	/** 
+	 * Lê e consulta o bit indicado pela mascara 
+	 * */
+	public  boolean readBit(int mask) {
+		read();
+		return isBit(mask);
+	}
+
+	/**
+	 * Coloca a Vcc os bits indicados pela mascara e os restantes bits mantêm o
+	 * valor.
+	 * 
+	 *  R | M | M + R  
+	 * ---------------
+	 *  0 | 0 |   0    <- Mantém
+	 *  0 | 1 |   1    <- Altera
+	 *  1 | 0 |   1    <- Mantém
+	 *  1 | 1 |   1    <- Altera
+	 * 
+	 * Output=readInput+Mask
+	 */
+	public  void setBits(int mask) {
+		writeOutput=writeOutput|mask;
+		write(writeOutput,EIGHTBITS);
+	}
+
+	/**
+	 * Coloca a GND os bits indicados pela mascara e os restantes bits mantêm o
+	 * valor.
+	 * 
+	 *  R | ~M | M * R  
+	 * ---------------
+	 *  0 | 1  |   0    <- Altera (acaba por manter)
+	 *  0 | 0  |   0    <- Mantém
+	 *  1 | 1  |   0    <- Altera
+	 *  1 | 0  |   1    <- Mantém
+	 * 
+	 * Output=~Mask&readInput
+	 */
+	public  void resetBits(int mask) {
+		writeOutput=~mask & writeOutput;
+		write(writeOutput,EIGHTBITS);
+	}
+
+	/**
+	 * Inverte o valor dos bits indicados pela mascara e os restantes bits
+	 * mantêm o valor.
+	 * 
+	 *  R | M | M ^ R  
+	 * ---------------
+	 *  0 | 0 |   0    <- Mantém
+	 *  0 | 1 |   1    <- Altera
+	 *  1 | 0 |   1    <- Mantém
+	 *  1 | 1 |   0    <- Altera
+	 * 
+	 * Output=readInput^Mask
+	 */
+	public  void invertBits(int mask) {
+		writeOutput= writeOutput ^ mask;
+		write(writeOutput,EIGHTBITS);
+	}
+
+	/**
+	 * Altera os valores dos bits indicados pela mascara para os bits
+	 * correspondentes em 'value' 1-Vcc 0-GND
+	 */
+	public  void write(int values, int mask) {
+		writeOutput=values&mask;
+		UsbPort.out(~writeOutput);
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ trunk/Test/LcdProtocol.java
@@ -0,0 +1,98 @@
+import isel.leic.utils.Time;
+/*
+ * 
+ * 31401 - Nuno Cancelo 
+ * 31900 - José Guilherme
+ * 33595 - Nuno Sousa
+ * 
+ */
+
+public class LcdProtocol implements KitConstants{
+	private int	bits2Shift;
+	private Kit ourKit;
+	
+	public LcdProtocol(){
+		bits2Shift=4;
+		ourKit=new Kit();
+	}
+	/**
+	 * Cria a sequencia de inicio do Protocolo
+	 */
+	private  void initProtocol() {
+		ourKit.setBits(RxC_MASK |RxD_MASK);
+		fullDelay();
+	}
+
+	/**
+	 * Cria sequencia de Start.
+	 */
+	private  void setStart() {
+		ourKit.invertBits(RxD_MASK);
+		delay();
+	}
+	/**
+	 * Verifica se o Kit está pronto a envia a sequencia de Dados.
+	 * @return
+	 */
+	private  boolean isReady() {
+		return ourKit.readBit(RxRDY_MASK);
+	}
+
+	/**
+	 * Faz reset à sequencia de dados.
+	 */
+	private  void resetProtocol() {
+		ourKit.setBits(RxD_MASK);
+	}
+
+	/**
+	 * Temporizador que pára o acesso ao Kit.
+	 */
+	private  void delay() {
+		Time.sleep(HALFCLOCK_MASK);
+	}
+	/**
+	 * Temporizador que pára o acesso ao Kit.
+	 */
+	private  void fullDelay() {
+		Time.sleep(CLOCK_MASK);
+	}
+	/**
+	 * Envia a sequencia de dados para ser mostrado no LCD. 
+	 * @param rs
+	 * @param value
+	 */
+	public  void sendBits(int rs, int value) {
+		initProtocol();
+		if (isReady()) {
+			setStart();
+			sendBit(rs);
+			
+			for (int count=bits2Shift;count!=0;count-=1){
+				sendBit(value);
+				value = value >> SHIFT_BITS_MASK;
+			}
+
+		}
+	}
+
+	/**
+	 * Envia os dados, segundo o protocolo, pelo Output port.
+	 * @param value
+	 */
+	private  void sendBit(int value) {
+		ourKit.invertBits(RxC_MASK);
+		ourKit.write(value,RxD_MASK);
+		delay();
+		ourKit.setBits(RxC_MASK);
+		delay();
+	}
+
+	/**
+	 * @param args
+	 */
+	public  void main(String[] args) {
+
+	}
+
+}
