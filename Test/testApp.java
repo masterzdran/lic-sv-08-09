486a244f594793c01519dc37f91ce4ae154428d9
Nuno.Cancelo <Nuno.Cancelo@98694540-1dcb-11de-b650-e715bd6d7cf1>
2009-07-20 09:39:18 +0000

    Organização de Pastas (Passo 3)

---
+++ Test/testApp.java
@@ -0,0 +1,54 @@
+
+
+/*
+ * 
+ * 31401 - Nuno Cancelo 
+ * 31900 - José Guilherme
+ * 33595 - Nuno Sousa
+ * 
+ */
+public class testApp implements AccessControlConstants {
+
+	public static void main(String[] args) {
+		// // TODO Auto-generated method stub
+//		 LCD l=new LCD();
+//		 Keyboard k=new Keyboard();
+//		 l.setCenter(true);
+//		 l.blinkOff();
+//		 l.writeLine(0,ASK4USER );
+//		 int x=l.getPos(ASK4USER, PINNBR);
+//		 l.posCursor(0, x);
+//		
+//				
+//		 char key=0;
+//		 int c=0;
+//		 while(c<PINNBR){
+//		 key=k.waitKey(10000);
+//		 System.out.println(key);
+//		 l.write('*');
+//		 c++;
+//		 }
+//		 key=k.waitKey(10000);
+//		 switch (key) {
+//		 case 'C':
+//		 l.writeLine(1, "Code Canceled");
+//		 Time.sleep(2000);
+//		 l.clear();
+//		 l.writeLine(0,ASK4PIN );
+//		 l.posCursor(0, x);
+//		 break;
+//		 case 'E':
+//		 l.writeLine(1, "Code Inserted");
+//		 Time.sleep(2000);
+//		 break;
+//		 default:
+//		 break;
+//		 }
+//		 Time.sleep(5000);
+//		 l.displayControlOff();
+//		
+//		 }
+
+	}
+}
+
